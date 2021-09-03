3712eb6b4a164c52fc3414da91f4e9d84ac1f348
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2021-09-03 22:09:36 +0530

    inital commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void solve() {
+}
+
+int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int tc = 1;
+  // cin >> tc;
+  for (int t = 1; t <= tc; t++) {
+    // cout << "Case #" << t  << ": ";
+    solve();
+  }
+}
